bca8020ca563bd584e0b21fe74ba8c09bec0b6c1
Juan Pablo Inzunza Alfaro <jpinzunzaa@gmail.com>
2025-02-23 10:55:40 -0700

    Added refactor

--- html-games/1-animación/index.js
+++ html-games/1-animación/index.js
@@ -3,55 +3,52 @@ const ctx = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 600;
 
-const radio = 10;
-let bolax = 50;
-let bolay = 100;
-let dx = 4;
-let dy = 4;
-let width = canvas.width;
-let height = canvas.height;
-let right_limit= width - radio;
-let left_limit = radio;
-let top_limit = radio;
-let bottom_limit = height - radio;
+const check_limits = (ball) => {
+  let next_x = ball.x + ball.dx;
+  let next_y = ball.y + ball.dy;
+  let right_limit = ball.width - ball.radius;
+  let left_limit = ball.radius;
+  let top_imit = ball.radius;
+  let bottom_limit = ball.height - ball.radius;
 
-ctx.lineWidth = radio;
-ctx.fillStyle = 'red';
+  if (next_x > right_limit || next_x < left_limit) {
+    ball.dx *= -1;
+  }
+  if (next_y > bottom_limit || next_y < top_imit) {
+    ball.dy *= -1;
+  }
 
-const check_limits = () => {
-  let nbolax = bolax + dx;
-  let nbolay = bolay + dy;
-  if (nbolax > right_limit) {
-    dx *= -1;
-    nbolax = right_limit;
-  }
-  if (nbolax < left_limit) {
-    dx *= -1;
-    nbolax = left_limit;
-  }
-  if (nbolay > bottom_limit) {
-    dy *= -1;
-    nbolay = bottom_limit;
-  }
-  if (nbolay < top_limit) {
-    dy *= -1;
-    nbolay = top_limit;
-  }
-  bolax = nbolax;
-  bolay = nbolay;
+  ball.x += ball.dx;
+  ball.y += ball.dy;
 }
 
-const move = () => {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  check_limits();
-  ctx.beginPath();
-  ctx.arc(bolax, bolay, radio, 0, 2 * Math.PI, true);
-  ctx.fill();
+class Ball {
+  constructor(ctx, x, y, radius, dx, dy, color = 'red') {
+    this.ctx = ctx;
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.dx = dx;
+    this.dy = dy;
+    this.color = color;
+  }
+
+  move() {
+    this.ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    check_limits(this);
+    this.draw();
+  }
+
+  draw() {
+    this.ctx.beginPath();
+    this.ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+    this.ctx.fillStyle = this.color;
+    this.ctx.fill();
+  }
 }
 
-setInterval(
-  () => {
-    move();
-  },
-  60
-)
+const ball = new Ball(ctx, 50, 100, 10, 4, 4);
+
+setInterval(() => {
+  ball.move();
+}, 60);
